c46feaf09525bf3fd95d20137b89c1c8cccaaf3f
Janhavimishra26 <janhavim674@gmail.com>
2026-06-09 21:20:43 +0530

    update backend URL

--- backend/db.js
+++ backend/db.js
@@ -3,15 +3,18 @@ require('dotenv').config();
 
 // Create a connection pool instead of a single connection
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'expense_tracker',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+   ssl: {
+    rejectUnauthorized: false
+}
 });
-
 // Verify the connection pool works on startup
 db.getConnection((err, connection) => {
     if (err) {
